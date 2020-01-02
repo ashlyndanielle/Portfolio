ec2820190bd13d246951337bbe05deec9f981faf
Ashlyn Mitros <ashlyn_mitros@Master-of-the-Universe-2.local>
2020-01-02 12:41:59 -0700

    added blog page - just need to restart VScode...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Hello!</h1>
+      <h2>Stuff and things</h2>
+    </div>
+  )
+}
+
+export default IndexPage;
