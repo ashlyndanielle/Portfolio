24b42bcfd5931c4e7ad201d41c3b4bedc82fee03
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-01-20 12:31:00 -0700

    added query to blog template to populate each post

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
-const Blog = ({ pageContext }) => {
+// this is just the way it works...you have to 
+// export the query and then graphql does some
+// magic shit and this query has access to the context
+// passed into create page and badabing badaboom
+// you have access to your data via props.data
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark (
+      fields: {
+        slug: {
+          eq: $slug
+        }
+      }
+    ) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const Blog = (props) => {
+  const { frontmatter, html } = props.data.markdownRemark
   return (
     <Layout>
-      You are viewing the blog template
+      <h1>{frontmatter.title}</h1>
+      <h4>{frontmatter.date}</h4>
+      <div dangerouslySetInnerHTML={{__html: html}}></div>
     </Layout>
   )
 }
