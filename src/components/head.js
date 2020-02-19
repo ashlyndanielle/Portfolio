925341fcc1a4fa3fb971bf07bb33022fdbb2dffa
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-02-19 16:16:53 -0700

    added reference image and description

--- src/components/head.js
+++ src/components/head.js
@@ -13,7 +13,12 @@ const Head = ({ title }) => {
     }
   `)
   return (
-    <Helmet title={`${title} | ${data.site.siteMetadata.author}`} />
+    // <Helmet title={`${title} | ${data.site.siteMetadata.author}`} />
+    <Helmet>
+      <title>{title} | {data.site.siteMetadata.author}</title>
+      <meta property="og:image" content="https://media-exp1.licdn.com/dms/image/C5603AQGuGeNNJrcV0w/profile-displayphoto-shrink_200_200/0?e=1587600000&v=beta&t=jyUM20CU3q9XTOlF9OyozjhEOW_oBH0DtYSsG7tTZ6Y"></meta>
+      <meta property="og:description" content="Front End Web Developer based in Salt Lake City, Utah."></meta>
+    </Helmet>
   )
 }
 
