ef592749c99ac5127c4ad9304c4131db791442f5
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-03-18 15:58:12 -0600

    displaying last checked

--- src/pages/coronavirus.js
+++ src/pages/coronavirus.js
@@ -6,6 +6,7 @@ import Layout from '../components/layout';
 
 const Coronavirus = () => {
   const [virusData, setVirusData] = useState([]);
+  const [lastChecked, setLastChecked] = useState();
 
   useEffect(() => {
     axios.get('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats', {
@@ -13,12 +14,21 @@ const Coronavirus = () => {
         "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
         "x-rapidapi-key": coronavirusKey
       }
-    }).then(response => console.log(response))
+    }).then(response => {
+      console.log(response.data.data)
+      displayDate(response.data.data.lastChecked);
+    })
   })
+
+  function displayDate(date) {
+    let dateString = new Date(date);
+    setLastChecked(dateString.toLocaleString())
+  }
 
   return (
     <Layout>
       <h1>Current Coronavirus Data</h1>
+      <h2>{lastChecked}</h2>
     </Layout>
   )
 }
