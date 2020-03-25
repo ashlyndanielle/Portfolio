6266dce408e54b04b7a824051d4db6ecd1086e2b
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-03-24 21:35:20 -0600

    displaying intial data list

--- src/pages/coronavirus.js
+++ src/pages/coronavirus.js
@@ -5,8 +5,10 @@ import axios from 'axios';
 import Layout from '../components/layout';
 
 const Coronavirus = () => {
+  const [lastChecked, setLastChecked] = useState('');
+  const [totalInfected, setTotalInfected] = useState(0);
   const [virusData, setVirusData] = useState([]);
-  const [lastChecked, setLastChecked] = useState();
+  const [hasFetched, setHasFetched] = useState(false);
 
   useEffect(() => {
     axios.get('https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats', {
@@ -15,19 +17,49 @@ const Coronavirus = () => {
         "x-rapidapi-key": process.env.GATSBY_CORONAVIRUS_KEY
       }
     }).then(response => {
-      console.log(response.data.data)
-      displayDate(response.data.data.lastChecked);
+      const { lastChecked, covid19Stats } = response.data.data;
+      displayDate(lastChecked);
+      setVirusData(covid19Stats);
+      setHasFetched(true);
     })
-  })
+  }, []);
+
+  console.log('virus data: ', virusData);
 
   function displayDate(date) {
     let dateString = new Date(date);
     setLastChecked(dateString.toLocaleString())
   }
 
+  function displayTotals(infected, deaths) {
+    return <h3>Total Infected: {infected} || Total Deaths: {deaths}</h3>
+  }
+
+  const locationData = virusData.map(location => {
+    let infected = 0;
+    let deaths = 0;
+    if (location.country === "US" && location.province === "Utah") {
+      if (location.confirmed !== 0) {
+        return (
+          <ul key={location.keyId}>
+            <li><strong>City:</strong> {location.city}</li>
+            <li><strong>Infected:</strong> {location.confirmed}</li>
+            <li><strong>Deaths:</strong> {location.deaths}</li>
+          </ul>
+        )
+      }
+    }
+  })
+
   return (
     <Layout>
-      <h1>Coronavirus Data as of: {lastChecked}</h1>
+      <h1>Coronavirus as of: {lastChecked}</h1>
+        {
+          hasFetched ? 
+            locationData 
+              : 
+            null
+        }
     </Layout>
   )
 }
