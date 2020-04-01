eaeae6882c831a9adea909e3f104dce870279212
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-03-31 22:55:01 -0600

    added form to contact page plus form styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import Header from './header';
 import Footer from './footer';
 
 import '../styles/index.scss';
+import '../styles/styleguide.scss';
 import layoutStyles from './styles/layout.module.scss';
 
 const Layout = (props) => {
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,14 +3,28 @@ import React from 'react';
 import Layout from '../components/layout';
 import Head from '../components/head';
 
+import contactStyles from './styles/contact.module.scss';
+
 const ContactPage = () => {
   return (
     <Layout>
       <Head title="Contact" />
       <h2>Contact Me</h2>
-      <p><strong>Address:</strong> Cottonwood Heights, Utah</p>
-      <p><strong>Email:</strong> ashlyndmitros@gmail.com</p>
-      <p><strong>Phone:</strong> (801) 879 - 2866</p>
+      <form name="contact" method="POST" netlify>
+        <div className={contactStyles.inputContainer}>
+          <input type="text" name="name" id="name" required></input>
+          <label for="name">Name</label>
+        </div>
+        <div className={contactStyles.inputContainer}>
+          <input type="text" name="email" id="email" required></input>
+          <label for="email">Email</label>
+        </div>
+        <div className={contactStyles.inputContainer}>
+          <input type="text" name="subject" id="subject" required></input>
+          <label for="subject">Subject</label>
+          <textarea name="message" id="message" placeholder="message" rows="7"></textarea>
+        </div>
+      </form>
     </Layout>
   )
 }
