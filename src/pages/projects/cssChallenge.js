dd60387447ed4053c2ed33ff5b70a52b1ead49d2
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-04-23 18:50:32 -0600

    fixed broken references and updated header styles

--- src/pages/projects/cssChallenge.js
+++ src/pages/projects/cssChallenge.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import challengeStyles from '../styles/cssChallenge.module.scss';
 
@@ -12,10 +13,10 @@ const CssChallenge = () => {
     <Layout>
       <Head title="100 Days of CSS"/>
       <Link to={'/projects'}>
-        <p className={projectStyles.breadcrumb}>&#8592; Projects</p>
+        <p className={challengeStyles.breadcrumb}>&#8592; Projects</p>
       </Link>
       <h1>100 Days of CSS Challenge</h1>
-      <p>Challenges curtosy of <a href="https://100dayscss.com/" target="_blank" rel="noopener">100dayscss.com</a></p>
+      <p>Challenges curtosy of <a href="https://100dayscss.com/" target="_blank" rel="noopener noreferrer">100dayscss.com</a></p>
       <div className={challengeStyles.container}>
         <BlinkingEye/>
       </div>
