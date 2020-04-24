4bf7f0b25dcbf447969f27866cf28e037c3d2ffd
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-04-23 23:59:43 -0600

    added hover button css challenge pen

--- src/pages/projects/cssChallenge.js
+++ src/pages/projects/cssChallenge.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby';
 
 import challengeStyles from '../styles/cssChallenge.module.scss';
 
-import Layout from '../../components/layout';
 import LayoutFullwidth from '../../components/layout-fullwidth';
 import Head from '../../components/head';
 
 import BlinkingEye from '../../components/cssChallenges/blinkingEye';
+import FlipflopHover from '../../components/cssChallenges/flipflopHover';
 
 const CssChallenge = () => {
   return (
@@ -20,6 +20,7 @@ const CssChallenge = () => {
       <p>Challenges courtesy of <a href="https://100dayscss.com/" target="_blank" rel="noopener noreferrer">100dayscss.com</a></p>
       <div className={challengeStyles.container}>
         <BlinkingEye/>
+        <FlipflopHover/>
       </div>
     </LayoutFullwidth>
   )
