8467038c3e351eca01c00ad44945f6f9177d807c
Ashlyn Mitros <ashlyndmitros@gmail.com>
2020-04-27 18:29:40 -0600

    fixed typo and grid container

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ const AboutPage = () => {
     <Layout>
       <Head title="About" />
       <h2>About</h2>
-      <p>I was born and raised in Salt Lake City, Utah and moved to Steamboat Springs, Colorado after high school. I spent a few years embracing life and enjoying the mountain town lifestyle: skiing, snowboarding, mountain biking, rock clibming, floating down the Yampa and just playing in the dirt! I moved back to Utah in 2013 to take some courses and be near my family (because they are awesome). It took a few more years but I finally stumbled on my passion.</p>
+      <p>I was born and raised in Salt Lake City, Utah and moved to Steamboat Springs, Colorado after high school. I spent a few years embracing life and enjoying the mountain town lifestyle: skiing, snowboarding, mountain biking, rock climbing, floating down the Yampa and just playing in the dirt! I moved back to Utah in 2013 to take some courses and be near my family (because they are awesome). It took a few more years but I finally stumbled on my passion.</p>
       <p>In the spring of 2017, I started looking into the world of web development and was immediately hooked. The combination of logic and creativity, endless opportunities to learn and grow, and ability to easily work from anywhere in the world pulled me in. It was love at first function.</p>
       <p>I started working at Sundance Institute that fall and I've strived to advance my skills everyday. I'm passionate and self driven, dedicated and determined, curious and resourceful, and a committed member of my team!</p>
     </Layout>
